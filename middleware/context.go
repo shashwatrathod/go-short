296c2e05d8a77ee0e61705c5db416252eb3f628c
Shashwat Rathod <shashwatrathod22@gmail.com>
2025-05-26 19:14:38 -0400

    feat: add Redis cache manager integration to context and update environment configuration

--- middleware/context.go
+++ middleware/context.go
@@ -4,23 +4,26 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/shashwatrathod/url-shortner/cache"
 	"github.com/shashwatrathod/url-shortner/core"
 	"github.com/shashwatrathod/url-shortner/db"
 	"github.com/shashwatrathod/url-shortner/db/dao"
 )
 
 type AppEnv struct {
-	DBManager *db.ConnectionManager
-	UrlAliasDao dao.UrlAliasDao
+	DBManager        *db.ConnectionManager
+	UrlAliasDao      dao.UrlAliasDao
 	AliasingStrategy core.AliasingStrategy
+	CacheManager     cache.CacheManager
 }
 
-func NewAppEnv(dbManager *db.ConnectionManager) *AppEnv {
+func NewAppEnv(dbManager *db.ConnectionManager, cacheManager cache.CacheManager) *AppEnv {
 	return &AppEnv{
-        DBManager:   dbManager,
-        UrlAliasDao: dao.NewUrlAliasDao(dbManager),
+		DBManager:        dbManager,
+		UrlAliasDao:      dao.NewUrlAliasDao(dbManager),
 		AliasingStrategy: core.NewSimpleAliasingStrategy(),
-    } 
+		CacheManager:     cacheManager,
+	}
 }
 
 // define a custom context key type for context injection
@@ -31,9 +34,9 @@ const ContextAppEnvKey contextKey = "appEnv"
 
 func ContextMiddleware(appEnv *AppEnv) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            ctx := context.WithValue(r.Context(), ContextAppEnvKey, appEnv)
-            next.ServeHTTP(w, r.WithContext(ctx))
-        })
-    }
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), ContextAppEnvKey, appEnv)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
